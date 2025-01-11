b55c66ea14014f99b3e15afbf2c127cc763b8f66
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-11 22:47:44 +0900

    divide function into two files

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,27 +6,52 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 03:30:54 by nash              #+#    #+#             */
-/*   Updated: 2025/01/10 21:03:17 by nash             ###   ########.fr       */
+/*   Updated: 2025/01/11 22:44:01 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
 
 char *get_next_line(int fd)
 {
   char* buffer;
-  size_t bytes;
-  int bytes_read;
+  char* start;
+  char* temp;
+  size_t count;
 
   buffer = malloc(BUFFER_SIZE);
-  bytes = sizeof(buffer);
-  bytes_read = read(fd, buffer, bytes);
-  if (bytes_read == -1)
-  {
+  if (!buffer)
     return NULL;
+  start = buffer;
+  temp = get_next_char__malloc(fd);
+  count = 0;
+  while (temp && count < BUFFER_SIZE)
+  {
+    *buffer = *temp;
+    free(temp);
+    buffer++;
+    count++;
+    temp = get_next_char__malloc(fd);
   }
-  return buffer;
+
+  *buffer = '\0';
+  return start;
+}
+
+int main()
+{
+  int fd = open("test.txt", O_RDONLY);
+  if (fd < 0)
+  {
+    perror("open");
+    return 1;
+  }
+
+  char* texts = get_next_line(fd);
+  if (texts)
+  {
+    printf("%s\n", texts);
+  }
+  close(fd);
+  return 0;
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,10 +6,19 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 03:30:51 by nash              #+#    #+#             */
-/*   Updated: 2025/01/10 03:43:42 by nash             ###   ########.fr       */
+/*   Updated: 2025/01/11 22:29:38 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
+#endif
+
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
+#include <stdio.h>
+
+char* get_next_char__malloc(int fd);
+char *get_next_line(int fd);
