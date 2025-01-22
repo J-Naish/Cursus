8399b40227fad8c7c87e4ee5bd1c623ee1d8f364
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-22 23:04:05 +0900

    update header

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 03:30:51 by nash              #+#    #+#             */
-/*   Updated: 2025/01/22 22:52:29 by nash             ###   ########.fr       */
+/*   Updated: 2025/01/22 23:03:55 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -20,8 +20,11 @@
 # include <stdio.h>
 # include <stdbool.h>
 
-// char	*get_next_char__malloc(int fd);
+# define BUFFER_SIZE 42
+
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	*ft_calloc(size_t count, size_t size);
 bool	ft_includes_newline(char *s);
+
+#endif
