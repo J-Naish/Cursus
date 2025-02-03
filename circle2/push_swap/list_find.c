7c8ca8a3759ac299ba0950ce45faddb6f29cf3c7
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-03 14:14:40 +0900

    add test code to list find

--- circle2/push_swap/list_find.c
+++ circle2/push_swap/list_find.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 06:39:59 by nash              #+#    #+#             */
-/*   Updated: 2025/02/03 06:46:14 by nash             ###   ########.fr       */
+/*   Updated: 2025/02/03 14:14:31 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,13 @@ t_node	*get_min_node(t_list *list)
 	}
 	return (min);
 }
+
+// int main() {
+// 	t_list *list = create_test_list();
+
+// 	t_node *minNode = get_min_node(list);
+// 	putlist_as_stack(list);
+// 	ft_putstr("\n");
+// 	putnode(minNode);
+// 	ft_putstr("\n");
+// }
