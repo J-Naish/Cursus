d9543ed5e7f68ca0d21992ad30ff31b5dddb6764
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-04 04:03:01 +0900

    create get index func

--- rank2/push_swap/list_find.c
+++ rank2/push_swap/list_find.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 06:39:59 by nash              #+#    #+#             */
-/*   Updated: 2025/02/03 14:14:31 by nash             ###   ########.fr       */
+/*   Updated: 2025/02/04 04:02:43 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,25 @@ t_node	*get_min_node(t_list *list)
 	return (min);
 }
 
+int	get_index(t_list *list, t_node *node)
+{
+	int		i;
+	t_node	*current;
+
+	if (!list || !node)
+		return (-1);
+	i = 0;
+	current = list->sentinel->next;
+	while (current != list->sentinel)
+	{
+		if (current == node)
+			return (i);
+		i++;
+		current = current->next;
+	}
+	return (-1);
+}
+
 // int main() {
 // 	t_list *list = create_test_list();
 
@@ -38,4 +57,6 @@ t_node	*get_min_node(t_list *list)
 // 	ft_putstr("\n");
 // 	putnode(minNode);
 // 	ft_putstr("\n");
+// 	int minIndex = get_index(list, minNode);
+// 	printf("%d\n", minIndex);
 // }
