6962223cbd08526f43e558a0e9d7632de2feb503
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-16 06:46:21 +0900

    update put error func

--- rank2/push_swap/error.c
+++ rank2/push_swap/error.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 05:18:43 by nash              #+#    #+#             */
-/*   Updated: 2025/02/03 01:39:36 by nash             ###   ########.fr       */
+/*   Updated: 2025/02/16 06:45:10 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	put_error(void)
 {
-	ft_putstr("Error\n");
+	write(STDERR_FILENO, "Error\n", 6);
 }
 
 // int main()
