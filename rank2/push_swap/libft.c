f0ff88d2661ea12b21ff91e5d25e07aafd691aed
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-16 07:08:34 +0900

    Update libft.c

--- rank2/push_swap/libft.c
+++ rank2/push_swap/libft.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 06:11:22 by nash              #+#    #+#             */
-/*   Updated: 2025/02/03 01:38:30 by nash             ###   ########.fr       */
+/*   Updated: 2025/02/16 07:08:27 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
-	if (!s)
-		return (0);
 	i = 0;
 	while (s[i])
 		i++;
@@ -33,8 +31,6 @@ void	ft_putstr(const char *s)
 {
 	size_t	len;
 
-	if (!s)
-		return ;
 	len = ft_strlen(s);
 	write(STDOUT_FILENO, s, len);
 }
