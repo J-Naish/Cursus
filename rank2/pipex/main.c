ce369e66d0c5feb46dc1be28c4b5260864d836b3
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-17 04:07:11 +0900

    execute each process

--- rank2/pipex/main.c
+++ rank2/pipex/main.c
@@ -6,33 +6,45 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 22:50:35 by nash              #+#    #+#             */
-/*   Updated: 2025/02/17 03:50:16 by nash             ###   ########.fr       */
+/*   Updated: 2025/02/17 04:06:40 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-
-void	pipex()
+void	process_all(int argc, char **argv, char **envp)
 {
+	int		i;
 	int		pipefd[2];
 	pid_t	pid;
 
-	if (pipe(pipefd) == -1)
-		error_exit();
-	pid = fork();
-	if (pid == -1)
-		error_exit();
-	if (pid == 0)
+	i = 2;
+	(void) envp;
+	while (i < argc - 1)
 	{
-		close(pipefd[1]);
-		close(pipefd[0]);
+		if (pipe(pipefd) == -1)
+			error_exit();
+		pid = fork();
+		if (pid == -1)
+			error_exit();
+		if (pid == 0)
+		{
+			close(pipefd[0]);
+			close(pipefd[1]);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			close(pipefd[0]);
+			close(pipefd[1]);
+		}
+		i++;
 	}
-	else
+	i = 2;
+	while (i < argc - 1)
 	{
-		waitpid(pid, NULL, 0);
-		close(pipefd[0]);
-		close(pipefd[1]);
+		wait(NULL);
+		i++;
 	}
 }
 
@@ -50,6 +62,6 @@ int	main(int argc, char **argv, char **envp)
 	if (outfilefd < 0)
 		error_exit();
 	(void) envp;
-	pipex();
+	process_all(argc, argv, envp);
 	return (0);
 }
