a607752bc47d8cd489424aeb25722f6254268a0d
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-17 04:33:05 +0900

    check accessibility to bin before exec

--- rank2/pipex/exec_cmd.c
+++ rank2/pipex/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 03:56:49 by nash              #+#    #+#             */
-/*   Updated: 2025/02/17 04:22:35 by nash             ###   ########.fr       */
+/*   Updated: 2025/02/17 04:32:26 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	exec_cmd(char *cmd, char **envp)
 {
 	char	*args[2];
 
+	if (access(cmd, X_OK) != 0)
+		error_exit();
 	args[0] = cmd;
 	args[1] = NULL;
 	if (execve(cmd, args, envp) == -1)
