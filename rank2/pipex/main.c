7e1dbb6eb777126ea635a4b54857c0a7f0b25262
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-17 04:43:19 +0900

    integrate process all and main

--- rank2/pipex/main.c
+++ rank2/pipex/main.c
@@ -6,18 +6,28 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 22:50:35 by nash              #+#    #+#             */
-/*   Updated: 2025/02/17 04:40:45 by nash             ###   ########.fr       */
+/*   Updated: 2025/02/17 04:43:03 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	process_all(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
+	int		infilefd;
+	int		outfilefd;
 	int		i;
 	int		pipefd[2];
 	pid_t	pid;
 
+	if (argc < 5)
+		return (0);
+	infilefd = open(argv[1], O_RDONLY);
+	if (infilefd < 0)
+		error_exit();
+	outfilefd = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (outfilefd < 0)
+		error_exit();
 	i = 2;
 	while (i < argc - 1)
 	{
@@ -46,21 +56,5 @@ void	process_all(int argc, char **argv, char **envp)
 		wait(NULL);
 		i++;
 	}
-}
-
-int	main(int argc, char **argv, char **envp)
-{
-	int	infilefd;
-	int	outfilefd;
-
-	if (argc < 5)
-		return (0);
-	infilefd = open(argv[1], O_RDONLY);
-	if (infilefd < 0)
-		error_exit();
-	outfilefd = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if (outfilefd < 0)
-		error_exit();
-	process_all(argc, argv, envp);
 	return (0);
 }
