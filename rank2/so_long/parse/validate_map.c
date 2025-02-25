3e05818274672119a6c8b71dd3d3d317044272b4
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-26 08:07:01 +0900

    create basic of validate rect

--- rank2/so_long/parse/validate_map.c
+++ rank2/so_long/parse/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 04:51:18 by nash              #+#    #+#             */
-/*   Updated: 2025/02/26 07:30:51 by nash             ###   ########.fr       */
+/*   Updated: 2025/02/26 08:06:38 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,18 +71,88 @@ static void	validate_mapsize(const char *map)
 	}
 }
 
+size_t	count_new_line(char *s)
+{
+	size_t	count;
+	int		i;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+size_t	strarr_size(char **strarr)
+{
+	size_t	count;
+
+	while (strarr[count])
+		count++;
+	return (count);
+}
+
+size_t	linelen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	return (i);
+}
+
+void	validate_rect(const char *map)
+{
+	char	**lines;
+	size_t	size_lines;
+	size_t	new_line_count;
+	int		i;
+
+	lines = ft_split(map, '\n');
+	if (!lines)
+	{
+		ft_putstr_fd("Memory allocation failed.\n", STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	size_lines = strarr_size(lines);
+	new_line_count = count_new_line(map);
+	if (size_lines != new_line_count + 1)
+	{
+		ft_putstr_fd("Error\nToo many line feeds.\n", STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	while (i < size_lines - 1)
+	{
+		if (linelen(lines[i]) != linelen(lines[i + 1]))
+		{
+			ft_putstr_fd("Error\nMap must be rect.\n", STDERR_FILENO);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+}
+
 void	validate_map(const char *map)
 {
 	validate_char(map);
 	validate_mapsize(map);
 	validate_composition(map);
-	// row length validate
-	// col length validate
+	// col length validate (col >= 5, each col must be the same width)
+	// row length validate (row >= 3)
 	// wall validate
 }
 
 // int main() {
-// 	char *map = "11111111\n10000001\n10000001";
-// 	// linelen test
-// 	printf("%zu\n", linelen(map));
+// 	char *test = "\n\n1111\n1111\n1111\n";
+// 	char **lines = ft_split(test, '\n');
+// 	printf("%s\n", lines[0]);
+// 	printf("%s\n", lines[1]);
+// 	printf("%s\n", lines[2]);
+// 	printf("%s\n", lines[3]);
 // }
