a67d12bba4c5b6842b4bcf19b029db9c49e5efc2
J-Naish <mjyrgd2332@i.softbank.jp>
2025-03-01 05:24:11 +0900

    remove destroy game

--- rank2/so_long/end_play.c
+++ rank2/so_long/end_play.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 03:44:30 by nash              #+#    #+#             */
-/*   Updated: 2025/03/01 05:21:00 by nash             ###   ########.fr       */
+/*   Updated: 2025/03/01 05:23:47 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	end_play(t_game *game)
 {
-	destroy_game(game);
+	free(game->window);
+	free(game->mlx);
+	free(game->map);
 }
