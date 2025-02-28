8fb7509995b636f9f4caf969270be02a044c2dcb
J-Naish <mjyrgd2332@i.softbank.jp>
2025-03-01 00:42:26 +0900

    add code to initialize mlx and window

--- rank2/so_long/main.c
+++ rank2/so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 04:39:00 by nash              #+#    #+#             */
-/*   Updated: 2025/02/26 07:09:30 by nash             ###   ########.fr       */
+/*   Updated: 2025/03/01 00:41:58 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,21 @@
 int	main(int argc, char **argv)
 {
 	char	*map;
+	void	*mlx_ptr;
+	void	*win_ptr;
 
 	validate_arg(argc, argv);
 	map = read_map(argv[1]);
 	validate_map(map);
 	printf("%s\n", map);
+	mlx_ptr = mlx_init();
+	if (!mlx_ptr)
+		return (free(map), EXIT_FAILURE);
+	win_ptr = mlx_new_window(mlx_ptr, 600, 400, "hi :)");
+	if (!win_ptr)
+		return (free(map), free(mlx_ptr), EXIT_FAILURE);
+	free(map);
+	free(mlx_ptr);
+	free(win_ptr);
 	return (EXIT_SUCCESS);
 }
