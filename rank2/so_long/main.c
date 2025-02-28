132762b49a4a51c55cfbe557f701b4e7265eaee5
J-Naish <mjyrgd2332@i.softbank.jp>
2025-03-01 01:07:10 +0900

    update window size

--- rank2/so_long/main.c
+++ rank2/so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 04:39:00 by nash              #+#    #+#             */
-/*   Updated: 2025/03/01 01:04:15 by nash             ###   ########.fr       */
+/*   Updated: 2025/03/01 01:06:52 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int argc, char **argv)
 	mlx_ptr = mlx_init();
 	if (!mlx_ptr)
 		return (free(map), EXIT_FAILURE);
-	win_ptr = mlx_new_window(mlx_ptr, 600, 400, "so_long");
+	win_ptr = mlx_new_window(mlx_ptr, 1200, 800, "so_long");
 	if (!win_ptr)
 		return (free(map), free(mlx_ptr), EXIT_FAILURE);
 	mlx_loop(mlx_ptr);
