4c69df2903f1cefcbcfe4ea97ea42d5c963064e5
J-Naish <mjyrgd2332@i.softbank.jp>
2025-03-03 10:06:54 +0900

    rename mlx to minilibx-linux

--- rank2/so_long/so_long.h
+++ rank2/so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 04:39:12 by nash              #+#    #+#             */
-/*   Updated: 2025/03/03 09:26:48 by nash             ###   ########.fr       */
+/*   Updated: 2025/03/03 10:06:07 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <stdbool.h>
 # include <fcntl.h>
 # include <errno.h>
-# include "./mlx/mlx.h"
+# include "./minilibx-linux/mlx.h"
 # include "./libft/libft.h"
 
 # ifndef BUFFER_SIZE
