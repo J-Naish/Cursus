390f29fa3babbccca9c2c55d503ba7a8b32f23de
J-Naish <mjyrgd2332@i.softbank.jp>
2025-03-13 11:09:06 +0900

    Update thread.c

--- rank03/Philosophers/philo/thread.c
+++ rank03/Philosophers/philo/thread.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 09:33:27 by nash              #+#    #+#             */
-/*   Updated: 2025/03/13 10:57:01 by nash             ###   ########.fr       */
+/*   Updated: 2025/03/13 11:07:42 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ void	create_threads(t_table table)
 	i = 0;
 	while (i < table.config.num_philos)
 	{
-		print_philo(table.philos[i]);
 		pthread_create(&table.philos[i].tid, NULL, start_routine, &table);
+		print_philo(table.philos[i]);
 		i++;
 	}
 }
