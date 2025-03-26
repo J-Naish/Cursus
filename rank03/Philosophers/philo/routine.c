7d7427fa061af45758a68056e8b8f3b1feabd95b
J-Naish <mjyrgd2332@i.softbank.jp>
2025-03-26 23:08:58 +0900

    update the logic to take a fork

--- rank03/Philosophers/philo/routine.c
+++ rank03/Philosophers/philo/routine.c
@@ -6,24 +6,48 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 16:17:42 by nash              #+#    #+#             */
-/*   Updated: 2025/03/26 21:15:54 by nash             ###   ########.fr       */
+/*   Updated: 2025/03/26 23:08:22 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static void	philo_take_fork(t_philo *philo)
+{
+	if ((*philo).number % 2 == 0)
+	{
+		pthread_mutex_lock(&(*philo).r_fork->mutex);
+		log_take_fork(*philo);
+		pthread_mutex_lock(&(*philo).l_fork->mutex);
+		log_take_fork(*philo);
+	}
+	else
+	{
+		pthread_mutex_lock(&(*philo).l_fork->mutex);
+		log_take_fork(*philo);
+		pthread_mutex_lock(&(*philo).r_fork->mutex);
+		log_take_fork(*philo);
+	}
+}
+
 static void	philo_eat(t_philo *philo)
 {
-	pthread_mutex_lock(&(*philo).l_fork->mutex);
-	pthread_mutex_lock(&(*philo).r_fork->mutex);
-	log_take_fork(*philo);
+	philo_take_fork(philo);
 	log_eat(*philo);
 	gettimeofday(&(*philo).last_meal_time, NULL);
 	(*philo).state = EATING;
 	usleep((*philo).config.time_to_eat * 1000);
 	(*philo).eating_count++;
-	pthread_mutex_unlock(&(*philo).r_fork->mutex);
-	pthread_mutex_unlock(&(*philo).l_fork->mutex);
+	if ((*philo).number % 2 == 0)
+	{
+		pthread_mutex_unlock(&(*philo).l_fork->mutex);
+		pthread_mutex_unlock(&(*philo).r_fork->mutex);
+	}
+	else
+	{
+		pthread_mutex_unlock(&(*philo).r_fork->mutex);
+		pthread_mutex_unlock(&(*philo).l_fork->mutex);
+	}
 }
 
 static void	philo_sleep(t_philo *philo)
