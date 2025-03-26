211d2195e073f9f9651035bc41a4c2e1b57c1400
J-Naish <mjyrgd2332@i.softbank.jp>
2025-03-26 15:37:36 +0900

    update log funcs

--- rank03/Philosophers/philo/log.c
+++ rank03/Philosophers/philo/log.c
@@ -6,41 +6,41 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 01:37:26 by nash              #+#    #+#             */
-/*   Updated: 2025/03/25 20:05:06 by nash             ###   ########.fr       */
+/*   Updated: 2025/03/26 15:37:26 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	log_take_fork(struct timeval start_time, t_philo philo)
+void	log_take_fork(t_philo philo)
 {
 	struct timeval	now;
 
 	gettimeofday(&now, NULL);
 	printf("%d %d has taken a fork\n",
-		get_elapsed_time(start_time), philo.number);
+		get_elapsed_time(philo.start_time), philo.number);
 }
 
-void	log_eat(struct timeval start_time, t_philo philo)
+void	log_eat(t_philo philo)
 {
 	printf("%d %d is eating\n",
-		get_elapsed_time(start_time), philo.number);
+		get_elapsed_time(philo.start_time), philo.number);
 }
 
-void	log_sleep(struct timeval start_time, t_philo philo)
+void	log_sleep(t_philo philo)
 {
 	printf("%d %d is sleeping\n",
-		get_elapsed_time(start_time), philo.number);
+		get_elapsed_time(philo.start_time), philo.number);
 }
 
-void	log_think(struct timeval start_time, t_philo philo)
+void	log_think(t_philo philo)
 {
 	printf("%d %d is thinking\n",
-		get_elapsed_time(start_time), philo.number);
+		get_elapsed_time(philo.start_time), philo.number);
 }
 
-void	log_died(struct timeval start_time, t_philo philo)
+void	log_died(t_philo philo)
 {
 	printf("%d %d died\n",
-		get_elapsed_time(start_time), philo.number);
+		get_elapsed_time(philo.start_time), philo.number);
 }
