67a89cf549e3b33d2dc5cd8cbc9f6c0293f2e123
J-Naish <mjyrgd2332@i.softbank.jp>
2025-03-26 16:35:37 +0900

    Update main.c

--- rank03/Philosophers/philo/main.c
+++ rank03/Philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 07:41:04 by nash              #+#    #+#             */
-/*   Updated: 2025/03/26 16:07:23 by nash             ###   ########.fr       */
+/*   Updated: 2025/03/26 16:35:32 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,7 @@ int	main(int argc, char **argv)
 	}
 	table = init_table(argc, argv);
 	create_threads(table);
-	
-	// Wait for all philosopher threads to finish
-	int i;
-	for (i = 0; i < table.config.num_philos; i++)
-	{
-		pthread_join(table.philos[i].tid, NULL);
-	}
-	
+	join_threads(table);
 	deinit_table(table);
 	return (EXIT_SUCCESS);
 }
