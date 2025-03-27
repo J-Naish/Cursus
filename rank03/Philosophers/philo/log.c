3ef5ee74a805126b7e47e3c54ebea0bef1a93216
J-Naish <mjyrgd2332@i.softbank.jp>
2025-03-28 03:56:52 +0900

    add early return to log funcs

--- rank03/Philosophers/philo/log.c
+++ rank03/Philosophers/philo/log.c
@@ -4,6 +4,8 @@ void	log_take_fork(t_philo philo)
 {
 	struct timeval	now;
 
+	if (!should_simulation_continue(philo))
+		return ;
 	gettimeofday(&now, NULL);
 	printf("%d %d has taken a fork\n",
 		get_elapsed_time(philo.start_time), philo.number);
@@ -11,18 +13,24 @@ void	log_take_fork(t_philo philo)
 
 void	log_eat(t_philo philo)
 {
+	if (!should_simulation_continue(philo))
+		return ;
 	printf(YELLOW "%d %d is eating\n" RESET,
 		get_elapsed_time(philo.start_time), philo.number);
 }
 
 void	log_sleep(t_philo philo)
 {
+	if (!should_simulation_continue(philo))
+		return ;
 	printf(CYAN "%d %d is sleeping\n" RESET,
 		get_elapsed_time(philo.start_time), philo.number);
 }
 
 void	log_think(t_philo philo)
 {
+	if (!should_simulation_continue(philo))
+		return ;
 	printf(GREEN "%d %d is thinking\n" RESET,
 		get_elapsed_time(philo.start_time), philo.number);
 }
