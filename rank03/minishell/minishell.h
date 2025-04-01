7cda6a2a2faf0a7b10e5022f23b71735a7787aa9
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-02 04:53:45 +0900

    Update minishell.h

--- rank03/minishell/minishell.h
+++ rank03/minishell/minishell.h
@@ -6,13 +6,12 @@
 /*   By: nash <nash@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 04:04:52 by nash              #+#    #+#             */
-/*   Updated: 2025/04/02 04:12:52 by nash             ###   ########.fr       */
+/*   Updated: 2025/04/02 04:53:32 by nash             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-
 
 # include "./libft/libft.h"
 
