f11734ce55247133e9c63d9293a0057fa9481451
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-02 19:36:07 +0900

    init table in main

--- rank03/Philosophers/philo_bonus/main_bonus.c
+++ rank03/Philosophers/philo_bonus/main_bonus.c
@@ -2,10 +2,15 @@
 
 int	main(int argc, char **argv)
 {
+	t_table	*table;
+
 	if (!is_valid_arg(argc, argv))
 	{
 		write(STDERR_FILENO, "Invalid args\n", 13);
 		return (EXIT_FAILURE);
 	}
+	table = init_table(argc, argv);
+	if (!table)
+		exit(EXIT_FAILURE);
 	return (0);
 }
