571c18b218f53319ce4d444299addf189d221d04
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-03 00:45:43 +0900

    Update log_bonus.c

--- rank03/Philosophers/philo_bonus/log_bonus.c
+++ rank03/Philosophers/philo_bonus/log_bonus.c
@@ -2,30 +2,30 @@
 
 void	log_take_fork(t_philo philo)
 {
-	printf("%d %d has taken a fork\n",
+	printf("%d %zu has taken a fork\n",
 		get_elapsed_time(philo.meta->start_time), philo.number);
 }
 
 void	log_eat(t_philo philo)
 {
-	printf(YELLOW "%d %d is eating\n" RESET,
+	printf(YELLOW "%d %zu is eating\n" RESET,
 		get_elapsed_time(philo.meta->start_time), philo.number);
 }
 
 void	log_sleep(t_philo philo)
 {
-	printf(CYAN "%d %d is sleeping\n" RESET,
+	printf(CYAN "%d %zu is sleeping\n" RESET,
 		get_elapsed_time(philo.meta->start_time), philo.number);
 }
 
 void	log_think(t_philo philo)
 {
-	printf(GREEN "%d %d is thinking\n" RESET,
+	printf(GREEN "%d %zu is thinking\n" RESET,
 		get_elapsed_time(philo.meta->start_time), philo.number);
 }
 
 void	log_died(t_philo philo)
 {
-	printf(RED "%d %d died\n" RESET,
+	printf(RED "%d %zu died\n" RESET,
 		get_elapsed_time(philo.meta->start_time), philo.number);
 }
