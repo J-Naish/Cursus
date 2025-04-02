220b1d6dbc219748edd73affc85a0cae5ad31bf0
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-02 10:29:06 +0900

    include headers

--- rank03/Philosophers/philo_bonus/philo_bonus.h
+++ rank03/Philosophers/philo_bonus/philo_bonus.h
@@ -1,4 +1,12 @@
 #ifndef PHILO_BONUS_H
 # define PHILO_BONUS_H
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <sys/time.h>
+# include <sys/wait.h>
+# include <semaphore.h>
+
 #endif
