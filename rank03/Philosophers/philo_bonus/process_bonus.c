86b16c05e82f48701381a059de0aaa46fa37f000
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-02 20:35:26 +0900

    Create process_bonus.c

---
+++ rank03/Philosophers/philo_bonus/process_bonus.c
@@ -0,0 +1,30 @@
+#include "philo_bonus.h"
+
+void	create_processes(t_table *table, void (*routine)(t_philo *))
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (i < table->config.num_philos)
+	{
+		table->philos[i].pid = fork();
+		if (table->philos[i].pid < 0)
+		{
+			j = 0;
+			while (j < i)
+			{
+				kill(table->philos[j].pid, SIGTERM);
+				j++;
+			}
+			deinit_table(table);
+			exit(EXIT_FAILURE);
+		}
+		else if (table->philos[i].pid == 0)
+		{
+			routine(&(table->philos[i]));
+			exit(EXIT_SUCCESS);
+		}
+		i++;
+	}
+}
