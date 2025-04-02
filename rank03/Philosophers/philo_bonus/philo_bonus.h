3c69756a3cfb16b19b1394505a85bfe04d9dd945
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-02 10:39:20 +0900

    define basic types

--- rank03/Philosophers/philo_bonus/philo_bonus.h
+++ rank03/Philosophers/philo_bonus/philo_bonus.h
@@ -8,5 +8,50 @@
 # include <sys/time.h>
 # include <sys/wait.h>
 # include <semaphore.h>
+# include <stdint.h>
+
+# ifndef RED
+#  define RED "\033[31m"
+# endif
+
+# ifndef YELLOW
+#  define YELLOW "\033[33m"
+# endif
+
+# ifndef CYAN
+#  define CYAN "\033[36m"
+# endif
+
+# ifndef GREEN
+#  define GREEN "\033[32m"
+# endif
+
+# ifndef RESET
+#  define RESET "\033[0m"
+# endif
+
+typedef enum e_philo_state
+{
+	THINKING = 0,
+	SLEEPING = 1,
+	EATING = 2,
+}	t_philo_state;
+
+typedef struct s_philo
+{
+	pid_t			pid;
+	t_philo_state	state;
+	size_t			eating_count;
+	struct timeval	last_meal_time;
+}	t_philo;
+
+typedef struct s_config
+{
+	size_t	num_philos;
+	size_t	time_to_die;
+	size_t	time_to_eat;
+	size_t	time_to_sleep;
+	size_t	times_to_eat_to_exit;
+}	t_config;
 
 #endif
