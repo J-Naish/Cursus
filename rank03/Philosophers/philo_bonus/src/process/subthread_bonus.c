fe3e6649d5a6693ba86a26a9c4d46d716d7716ea
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-04 07:47:21 +0900

    handle meal semaphore in subthread monitor

--- rank03/Philosophers/philo_bonus/src/process/subthread_bonus.c
+++ rank03/Philosophers/philo_bonus/src/process/subthread_bonus.c
@@ -11,11 +11,13 @@ static bool	is_starving(t_philo philo)
 		return (false);
 }
 
-static void	*death_monitor_routine(void *arg)
+static void	*subthread_routine(void *arg)
 {
 	t_philo	*philo;
+	bool	has_eaten_enough;
 
 	philo = (t_philo *)arg;
+	has_eaten_enough = false;
 	while (1)
 	{
 		if (is_starving(*philo))
@@ -23,13 +25,19 @@ static void	*death_monitor_routine(void *arg)
 			log_died(*philo);
 			exit(EXIT_SUCCESS);
 		}
+		if (!has_eaten_enough
+			&& philo->eating_count > philo->meta->config.times_to_eat_to_exit)
+		{
+			has_eaten_enough = true;
+			sem_post(philo->meta->sem_meals);
+		}
 		usleep(1000);
 	}
 	return (NULL);
 }
 
-void	create_death_monitor_thread(t_philo *philo)
+void	create_subthread(t_philo *philo)
 {
-	pthread_create(&(philo->death_thread), NULL, death_monitor_routine, philo);
-	pthread_detach(philo->death_thread);
+	pthread_create(&(philo->subthread), NULL, subthread_routine, philo);
+	pthread_detach(philo->subthread);
 }
