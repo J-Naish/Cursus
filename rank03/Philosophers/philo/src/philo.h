bd2597a851204309587f76fb3ed78f5069baf9b5
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-05 01:51:43 +0900

    define data structure

--- rank03/Philosophers/philo/src/philo.h
+++ rank03/Philosophers/philo/src/philo.h
@@ -11,4 +11,79 @@
 # include <sys/types.h>
 # include <pthread.h>
 
+# ifndef RED
+#  define RED "\033[31m"
+# endif
+
+# ifndef YELLOW
+#  define YELLOW "\033[33m"
+# endif
+
+# ifndef WHITE
+#  define WHITE "\033[37m"
+# endif
+
+# ifndef CYAN
+#  define CYAN "\033[36m"
+# endif
+
+# ifndef GREEN
+#  define GREEN "\033[32m"
+# endif
+
+# ifndef MAGENTA
+#  define MAGENTA "\033[35m"
+# endif
+
+# ifndef RESET
+#  define RESET "\033[0m"
+# endif
+
+typedef struct s_config
+{
+	size_t	num_philos;
+	size_t	time_to_die;
+	size_t	time_to_eat;
+	size_t	time_to_sleep;
+	size_t	times_to_eat_to_exit;
+}	t_config;
+
+typedef struct s_meta
+{
+	struct timeval	start_time;
+	t_config		config;
+}	t_meta;
+
+typedef enum e_philo_state
+{
+	THINKING = 0,
+	SLEEPING = 1,
+	EATING = 2,
+}	t_philo_state;
+
+typedef struct s_fork
+{
+	size_t			number;
+	pthread_mutex_t	mutex;
+}	t_fork;
+
+typedef struct s_philo
+{
+	size_t			number;
+	t_philo_state	state;
+	size_t			eating_count;
+	struct timeval	last_meal_time;
+	pthread_t		tid;
+	t_fork			*l_fork;
+	t_fork			r_fork;
+	t_meta			*meta;
+}	t_philo;
+
+typedef struct s_table
+{
+	t_philo	*philos;
+	t_fork	*forks;
+	t_meta	*meta;
+}	t_table;
+
 #endif
