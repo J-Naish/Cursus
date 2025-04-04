4dfcbd12540222c94dd147c816b14a72d9146034
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-05 00:01:08 +0900

    create basic of philo

---
+++ rank03/Philosophers/philo/src/philo.h
@@ -0,0 +1,14 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <limits.h>
+# include <unistd.h>
+# include <time.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <pthread.h>
+
+#endif
