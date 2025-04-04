5b5ff618ac4de280fb3bae9f35467f0f5f8e520a
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-05 06:06:02 +0900

    create helper funcs

---
+++ rank03/Philosophers/philo/src/thread/helpers.c
@@ -0,0 +1,24 @@
+#include "../philo.h"
+
+bool	get_is_simulating(t_monitor *monitor)
+{
+	bool	b;
+
+	pthread_mutex_lock(&(monitor->mutex));
+	b = monitor->is_simulating;
+	pthread_mutex_unlock(&(monitor->mutex));
+	return (b);
+}
+
+void	split_sleep(size_t duration, t_monitor *monitor)
+{
+	struct timeval	start;
+
+	start = get_current_time();
+	while (get_elapsed_time(start) <= duration)
+	{
+		if (!get_is_simulating(monitor))
+			return ;
+		usleep(1000);
+	}
+}
