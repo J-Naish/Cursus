3eaaa275b36cc42455191effa83212539a431b23
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-05 06:48:17 +0900

    skip logging actions when simulation should stop

--- rank03/Philosophers/philo/src/thread/log.c
+++ rank03/Philosophers/philo/src/thread/log.c
@@ -2,24 +2,32 @@
 
 void	log_take_fork(t_philo philo)
 {
+	if (should_simulation_stop(philo.meta->monitor))
+		return ;
 	printf(WHITE"%d %lu has taken a fork\n"RESET,
 		get_elapsed_time(philo.meta->start_time), philo.number);
 }
 
 void	log_eat(t_philo philo)
 {
+	if (should_simulation_stop(philo.meta->monitor))
+		return ;
 	printf(YELLOW"%d %lu is eating\n"RESET,
 		get_elapsed_time(philo.meta->start_time), philo.number);
 }
 
 void	log_sleep(t_philo philo)
 {
+	if (should_simulation_stop(philo.meta->monitor))
+		return ;
 	printf(CYAN"%d %lu is sleeping\n"RESET,
 		get_elapsed_time(philo.meta->start_time), philo.number);
 }
 
 void	log_think(t_philo philo)
 {
+	if (should_simulation_stop(philo.meta->monitor))
+		return ;
 	printf(GREEN"%d %lu is thinking\n"RESET,
 		get_elapsed_time(philo.meta->start_time), philo.number);
 }
