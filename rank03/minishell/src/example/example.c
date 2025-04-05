ce847de2b3ec2971f6c8e29da7f00f03ba2212c4
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-06 01:44:39 +0900

    create example files

---
+++ rank03/minishell/src/example/example.c
@@ -0,0 +1,21 @@
+#include "../minishell.h"
+
+static int	get_square(int x)
+{
+	return (x * x);
+}
+
+int	example(int x)
+{
+	return (get_square(x) + 42);
+}
+
+// // テストコード例
+// #include <stdio.h>
+// int main() {
+// 	printf("%d\n", example(10));
+// 	printf("%d\n", example(0));
+// 	printf("%d\n", example(-1));
+// 	printf("%d\n", example(1234));
+// 	printf("%d\n", example(-567));
+// }
