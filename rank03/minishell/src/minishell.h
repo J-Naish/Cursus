cbd114393a0a2fc727ea65db7556c0813a9b01ef
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-06 08:38:51 +0900

    define exit statuse macros

--- rank03/minishell/src/minishell.h
+++ rank03/minishell/src/minishell.h
@@ -17,6 +17,18 @@
 # include "../libft/libft.h"
 
 
+# ifndef EXIT_UNABLE_TO_EXECUTE
+#  define EXIT_UNABLE_TO_EXECUTE 126
+# endif
+
+# ifndef EXIT_NOT_FOUND
+#  define EXIT_NOT_FOUND 127
+# endif
+
+# ifndef EXIT_SIGNAL_BASE
+#  define EXIT_SIGNAL_BASE 128
+# endif
+
 
 // MARK: example/
 // example.c
