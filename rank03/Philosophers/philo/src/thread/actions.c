e8bb721d036325c204730ea06f9501fa4d6ce0fc
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-14 12:31:01 +0900

    remove taking fork order logic

--- rank03/Philosophers/philo/src/thread/actions.c
+++ rank03/Philosophers/philo/src/thread/actions.c
@@ -4,27 +4,17 @@ static void	philo_take_fork(t_philo *philo)
 {
 	if (should_simulation_stop(philo->meta->monitor))
 		return ;
-	if (philo->number % 2 == 0)
+	pthread_mutex_lock(&(philo->r_fork->mutex));
+	log_take_fork(philo);
+	if (philo->meta->config.num_philos == 1)
 	{
-		pthread_mutex_lock(&(philo->l_fork->mutex));
-		log_take_fork(philo);
-		pthread_mutex_lock(&(philo->r_fork->mutex));
-		log_take_fork(philo);
+		while (!should_simulation_stop(philo->meta->monitor))
+			usleep(1000);
+		pthread_mutex_unlock(&(philo->r_fork->mutex));
+		return ;
 	}
-	else
-	{
-		pthread_mutex_lock(&(philo->r_fork->mutex));
-		log_take_fork(philo);
-		if (philo->meta->config.num_philos == 1)
-		{
-			while (!should_simulation_stop(philo->meta->monitor))
-				usleep(1000);
-			pthread_mutex_unlock(&(philo->r_fork->mutex));
-			return ;
-		}
-		pthread_mutex_lock(&(philo->l_fork->mutex));
-		log_take_fork(philo);
-	}
+	pthread_mutex_lock(&(philo->l_fork->mutex));
+	log_take_fork(philo);
 }
 
 void	philo_eat(t_philo *philo)
@@ -42,16 +32,8 @@ void	philo_eat(t_philo *philo)
 	pthread_mutex_lock(&(philo->mutex_eating_count));
 	philo->eating_count += 1;
 	pthread_mutex_unlock(&(philo->mutex_eating_count));
-	if (philo->number % 2 == 0)
-	{
-		pthread_mutex_unlock(&(philo->l_fork->mutex));
-		pthread_mutex_unlock(&(philo->r_fork->mutex));
-	}
-	else
-	{
-		pthread_mutex_unlock(&(philo->r_fork->mutex));
-		pthread_mutex_unlock(&(philo->l_fork->mutex));
-	}
+	pthread_mutex_unlock(&(philo->r_fork->mutex));
+	pthread_mutex_unlock(&(philo->l_fork->mutex));
 }
 
 void	philo_sleep(t_philo *philo)
