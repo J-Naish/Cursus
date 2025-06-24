eeafc8ed97b76eb9c1fca9156efe6cddd0cdc5cf
J-Naish <mjyrgd2332@i.softbank.jp>
2025-06-24 21:49:43 +0900

    complete ex06

---
+++ rank04/CPP Module 01/ex06/main.cpp
@@ -0,0 +1,14 @@
+#include "Harl.hpp"
+
+int main(int argc, char *argv[]) {
+
+    Harl harl = Harl();
+
+    if (argc >= 2) {
+        harl.complain(argv[1]);
+    } else {
+        harl.complain("");
+    }
+
+    return 0;
+}
