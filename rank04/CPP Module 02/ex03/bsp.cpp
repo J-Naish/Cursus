1bc337c65c875e1aa34dbd3f6629ef6ffe8d5d2d
J-Naish <mjyrgd2332@i.softbank.jp>
2025-06-26 07:33:11 +0900

    implement bsp

---
+++ rank04/CPP Module 02/ex03/bsp.cpp
@@ -0,0 +1,23 @@
+#include "Vector.hpp"
+#include "Point.hpp"
+
+bool bsp( Point const a, Point const b, Point const c, Point const point) {
+
+    const Vector ab = Vector(a, b);
+    const Vector bc = Vector(b, c);
+    const Vector ca = Vector(c, a);
+
+    const Vector ap = Vector(a, point);
+    const Vector bp = Vector(b, point);
+    const Vector cp = Vector(c, point);
+
+
+    Fixed cross1 = ab.crossProduct(ap);
+    Fixed cross2 = bc.crossProduct(bp);
+    Fixed cross3 = ca.crossProduct(cp);
+
+
+    return
+        (cross1 > 0 && cross2 > 0 && cross3 > 0) ||
+        (cross1 < 0 && cross2 < 0 && cross3 < 0);
+}
