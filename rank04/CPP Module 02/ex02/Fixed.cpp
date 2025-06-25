53abc3d6a5ec61eecdc4c820990544b2e598ec68
J-Naish <mjyrgd2332@i.softbank.jp>
2025-06-26 06:06:32 +0900

    implement min and max static func

--- rank04/CPP Module 02/ex02/Fixed.cpp
+++ rank04/CPP Module 02/ex02/Fixed.cpp
@@ -120,3 +120,35 @@ std::ostream& operator<<(std::ostream& out, Fixed const& f) {
     out << f.toFloat();
     return out;
 }
+
+Fixed& Fixed::min(Fixed& f1, Fixed& f2) {
+    if (f1 <= f2) {
+        return f1;
+    } else {
+        return f2;
+    }
+}
+
+const Fixed& Fixed::min(const Fixed& f1, const Fixed& f2) {
+    if (f1 <= f2) {
+        return f1;
+    } else {
+        return f2;
+    }
+}
+
+Fixed& Fixed::max(Fixed& f1, Fixed& f2) {
+    if (f1 >= f2) {
+        return f1;
+    } else {
+        return f2;
+    }
+}
+
+const Fixed& Fixed::max(const Fixed& f1, const Fixed& f2) {
+    if (f1 >= f2) {
+        return f1;
+    } else {
+        return f2;
+    }
+}
--- rank04/CPP Module 02/ex02/Fixed.hpp
+++ rank04/CPP Module 02/ex02/Fixed.hpp
@@ -39,6 +39,11 @@ class Fixed {
         float toFloat( void ) const;
         int toInt( void ) const;
 
+        static Fixed& min(Fixed& f1, Fixed& f2);
+        static const Fixed& min(const Fixed& f1, const Fixed& f2);
+        static Fixed& max(Fixed& f1, Fixed& f2);
+        static const Fixed& max(const Fixed& f1, const Fixed& f2);
+
     private:
 
         int raw_bits;
