a28ce50e5cfd7a9297ea0c20c06ee96a390ea8a6
J-Naish <mjyrgd2332@i.softbank.jp>
2025-06-26 04:26:58 +0900

    complete ex01

---
+++ rank04/CPP Module 02/ex01/Fixed.cpp
@@ -0,0 +1,52 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+    std::cout << "Default constructor called" << std::endl;
+    this->value = 0;
+}
+
+Fixed::Fixed(const int value) {
+    std::cout << "Int constructor called" << std::endl;
+    this->value = (value << Fixed::num_fractional_bits);
+}
+
+Fixed::Fixed(const float value) {
+    std::cout << "Float constructor called" << std::endl;
+    this->value = roundf(value * (1 << Fixed::num_fractional_bits));
+}
+
+Fixed::Fixed(const Fixed& other) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->value = other.getRawBits();
+    return *this;
+}
+
+Fixed::~Fixed(){
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const {
+    return this->value;
+}
+
+void Fixed::setRawBits( int const raw ) {
+    this->value = raw;
+}
+
+float Fixed::toFloat( void ) const {
+    return (float)this->value / (float)(1 << Fixed::num_fractional_bits);
+}
+
+int Fixed::toInt( void ) const {
+    return roundf(this->value / (1 << Fixed::num_fractional_bits));
+}
+
+std::ostream& operator<<(std::ostream& out, Fixed const& value) {
+    out << value.toFloat();
+    return out;
+}
---
+++ rank04/CPP Module 02/ex01/Fixed.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+
+class Fixed {
+
+    public:
+
+        Fixed();
+        Fixed(const int value);
+        Fixed(const float value);
+        Fixed(const Fixed& other);
+        Fixed& operator=(const Fixed& other);
+        Fixed& operator<<(const Fixed& value);
+        ~Fixed();
+
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+
+        float toFloat( void ) const;
+        int toInt( void ) const;
+
+    private:
+
+        int value;
+        static const int num_fractional_bits = 8;
+
+};
+
+
+std::ostream& operator<<(std::ostream& out, Fixed const& value);
+
+#endif
