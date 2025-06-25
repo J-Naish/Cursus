449178e819b3d66cb76c19f41724ea2b61720423
J-Naish <mjyrgd2332@i.softbank.jp>
2025-06-26 06:08:00 +0900

    remove debugging messages

--- rank04/CPP Module 02/ex02/Fixed.cpp
+++ rank04/CPP Module 02/ex02/Fixed.cpp
@@ -1,31 +1,24 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed() {
-    std::cout << "Default constructor called" << std::endl;
     this->raw_bits = 0;
 }
 
 Fixed::Fixed(const int value) {
-    std::cout << "Int constructor called" << std::endl;
     this->raw_bits = (value << Fixed::FRACTIONAL_BITS);
 }
 
 Fixed::Fixed(const float value) {
-    std::cout << "Float constructor called" << std::endl;
     this->raw_bits = roundf(value * Fixed::ONE);
 }
 
 Fixed::Fixed(const Fixed& other) {
-    std::cout << "Copy constructor called" << std::endl;
     *this = other;
 }
 
-Fixed::~Fixed(){
-    std::cout << "Destructor called" << std::endl;
-}
+Fixed::~Fixed(){}
 
 Fixed& Fixed::operator=(const Fixed& other) {
-    std::cout << "Copy assignment operator called" << std::endl;
     this->raw_bits = other.getRawBits();
     return *this;
 }
