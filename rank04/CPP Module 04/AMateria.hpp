ab7a3e04c834b479789c792bc9d3c3e4b64c18ce
J-Naish <mjyrgd2332@i.softbank.jp>
2025-06-28 06:16:01 +0900

    create basic of gives sources

---
+++ rank04/CPP Module 04/AMateria.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef AMATERIA_H
+#define AMATERIA_H
+
+#include <string>
+#include "ICharacter.hpp"
+
+class AMateria {
+    protected:
+
+
+    public:
+
+        AMateria();
+        AMateria(std::string const & type);
+        AMateria(const AMateria& other);
+        ~AMateria();
+        AMateria& operator=(const AMateria& other);
+
+        std::string const & getType() const;
+
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
+};
+
+#endif
