0a6203c38bcc47c8e015acd39e75ec9a92151c69
J-Naish <mjyrgd2332@i.softbank.jp>
2025-07-01 18:42:10 +0900

    define Bureaucrat class

---
+++ rank05/CPP Module 05/ex00/include/Bureaucrat.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#ifndef BUREAUCRAT_H_
+#define BUREAUCRAT_H_
+
+#include <string>
+#include <exception>
+
+
+class Bureaucrat {
+
+    public:
+
+        // constants
+        static const int    kHighestGrade = 1;
+        static const int    kLowestGrade = 150;
+
+
+        // constructors
+        Bureaucrat();
+        Bureaucrat(const std::string& name);
+        Bureaucrat(int grade);
+        Bureaucrat(const std::string& name, int grade);
+        Bureaucrat(const Bureaucrat& other);
+
+        // destructor
+        ~Bureaucrat();
+
+        // operator
+        Bureaucrat& operator=(const Bureaucrat& other);
+
+
+        // getters and setters
+        const std::string&  getName() const;
+        int                 getGrade() const;
+
+
+        // functions
+        void                incrementGrade();
+        void                decrementGrade();
+
+
+        // exceptions
+        class GradeTooHighException : public std::exception {
+            public:
+                const char* what() const throw();
+        };
+        class GradeTooLowException : public std::exception {
+            public:
+                const char* what() const throw();
+        };
+
+
+    private:
+
+        const std::string   name_;
+        int                 grade_;
+
+};
+
+
+#endif
