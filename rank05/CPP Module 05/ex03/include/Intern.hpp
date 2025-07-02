16d3469d6f80e36c28d0efcc9f5a20c8d137f8a7
J-Naish <mjyrgd2332@i.softbank.jp>
2025-07-02 18:54:12 +0900

    define intern class

---
+++ rank05/CPP Module 05/ex03/include/Intern.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef INTERN_H_
+#define INTERN_H_
+
+#include "AForm.hpp"
+
+class Intern {
+
+    public:
+
+        // constructors
+        Intern();
+        Intern(const Intern& other);
+
+        // destructor
+        ~Intern();
+
+        // operator
+        Intern& operator=(const Intern& other);
+
+        // member functions
+        AForm* makeForm(const std::string& form_name, const std::string form_target) const;
+
+};
+
+#endif
