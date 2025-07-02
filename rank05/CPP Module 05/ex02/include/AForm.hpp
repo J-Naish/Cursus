37cedc2a7a37c049b3be61afc72831f8d552f6db
J-Naish <mjyrgd2332@i.softbank.jp>
2025-07-02 16:12:54 +0900

    update form to abstruct form

---
+++ rank05/CPP Module 05/ex02/include/AForm.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#ifndef A_FORM_H_
+#define A_FORM_H_
+
+#include <string>
+#include <exception>
+#include <iostream>
+#include <sstream>
+#include "constants.hpp"
+
+class Bureaucrat;
+
+class AForm {
+
+
+    public:
+
+        // constructors
+        AForm();
+        AForm(const std::string& name);
+        AForm(int grade_to_sign, int grade_to_execute);
+        AForm(const std::string& name, int grade_to_sign, int grade_to_execute);
+        AForm(const AForm& other);
+
+        // destructor
+        ~AForm();
+
+        // operator
+        AForm& operator=(const AForm& other);
+
+
+        // getters
+        const std::string& getName() const;
+        bool               getIsSigned() const;
+        int                getGradeToSign() const;
+        int                getGradeToExecute() const;
+
+
+        // functions
+        void               beSigned(const Bureaucrat& bureaucrat);
+
+
+
+        // exceptions
+        class GradeTooHighException : public std::exception {
+            public:
+                const char* what() const throw();
+        };
+        class GradeTooLowException : public std::exception {
+            public:
+                const char* what() const throw();
+        };
+
+
+    private:
+
+        const std::string  name_;
+        bool               is_signed_;
+        const int          grade_to_sign_;
+        const int          grade_to_execute_;
+
+
+};
+
+std::ostream& operator<<(std::ostream& out, const AForm& f);
+
+#endif
---
+++ rank05/CPP Module 05/ex02/src/AForm.cpp
@@ -0,0 +1,115 @@
+#include "../include/AForm.hpp"
+#include "../include/Bureaucrat.hpp"
+
+// constructors
+AForm::AForm() :
+    name_("undefined"),
+    is_signed_(false),
+    grade_to_sign_(kHighestGrade),
+    grade_to_execute_(kHighestGrade) {}
+
+AForm::AForm(const std::string& name) :
+    name_(name),
+    is_signed_(false),
+    grade_to_sign_(kHighestGrade),
+    grade_to_execute_(kHighestGrade) {}
+
+AForm::AForm(int grade_to_sign, int grade_to_execute) :
+    name_("undefined"),
+    is_signed_(false),
+    grade_to_sign_(grade_to_sign),
+    grade_to_execute_(grade_to_execute)
+{
+    if (grade_to_sign < kHighestGrade || grade_to_execute < kHighestGrade) {
+        throw AForm::GradeTooHighException();
+    } else if (grade_to_sign_ > kLowestGrade || grade_to_execute_ > kLowestGrade) {
+        throw AForm::GradeTooLowException();
+    }
+}
+
+AForm::AForm(const std::string& name, int grade_to_sign, int grade_to_execute) :
+    name_(name),
+    is_signed_(false),
+    grade_to_sign_(grade_to_sign),
+    grade_to_execute_(grade_to_execute)
+{
+    if (grade_to_sign < kHighestGrade || grade_to_execute < kHighestGrade) {
+        throw AForm::GradeTooHighException();
+    } else if (grade_to_sign_ > kLowestGrade || grade_to_execute_ > kLowestGrade) {
+        throw AForm::GradeTooLowException();
+    }
+}
+
+AForm::AForm(const AForm& other) :
+    name_(other.getName()),
+    is_signed_(other.getIsSigned()),
+    grade_to_sign_(other.getGradeToSign()),
+    grade_to_execute_(other.getGradeToExecute()) {}
+
+
+// destructor
+AForm::~AForm() {}
+
+
+// operators
+AForm& AForm::operator=(const AForm& other) {
+    (void)other;
+    return *this;
+}
+
+
+// getters
+const std::string& AForm::getName() const {
+    return this->name_;
+}
+
+bool AForm::getIsSigned() const {
+    return this->is_signed_;
+}
+
+int AForm::getGradeToSign() const {
+    return this->grade_to_sign_;
+}
+
+int AForm::getGradeToExecute() const {
+    return this->grade_to_execute_;
+}
+
+
+
+// functions
+void AForm::beSigned(const Bureaucrat& bureaucrat) {
+    if (bureaucrat.getGrade() > this->grade_to_sign_) {
+        throw AForm::GradeTooLowException();
+    } else if (!this->is_signed_) {
+        this->is_signed_ = true;
+    }
+}
+
+
+
+// exceptions
+const char* AForm::GradeTooHighException::what() const throw() {
+    std::stringstream ss;
+    ss << "Form grade is too high (must be between " << kHighestGrade << " and " << kLowestGrade << ")";
+    static std::string msg = ss.str();
+    return msg.c_str();
+}
+
+const char* AForm::GradeTooLowException::what() const throw() {
+    std::stringstream ss;
+    ss << "Form grade is too low (must be between " << kHighestGrade << " and " << kLowestGrade << ")";
+    static std::string msg = ss.str();
+    return msg.c_str();
+}
+
+
+
+// << stream
+std::ostream& operator<<(std::ostream& out, const AForm& f) {
+    out << f.getName() << " form"
+        << ", `is signed`: " << f.getIsSigned()
+        << ", `grade to sign`: " << f.getGradeToSign()
+        << ", `grade to execute`: " << f.getGradeToExecute();
+    return out;
+}
